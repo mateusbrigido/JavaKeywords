a37f0c36b409c1f860b2bc08a6b0820db11d1e9e
Mateus Brigido <mateusbrigido@live.com>
2019-11-30 11:53:28 -0300

    Check the answers and insert the right answers in the tableview

--- JavaKeywords/QuestionViewController.swift
+++ JavaKeywords/QuestionViewController.swift
@@ -3,7 +3,6 @@ import UIKit
 class QuestionViewController: UIViewController {
 
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var answerTextField: UITextField!
     @IBOutlet weak var answersTableView: UITableView!
     
     @IBOutlet weak var scoreLabel: UILabel!
@@ -24,6 +23,9 @@ class QuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.answersTableView.dataSource = self
+        self.answersTableView.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,5 +35,29 @@ class QuestionViewController: UIViewController {
         }
     }
     
+    @IBAction func textfieldEdited(_ sender: UITextField) {
+        guard let text = sender.text, text.count > 0 else { return }
+
+        if let matchedText = question.answers.first(where: { $0 == text }) {
+            rightAnswers.insert(matchedText, at: 0)
+            sender.text = ""
+            answersTableView.reloadData()
+            question.answers.remove(matchedText)
+        }
+    }
+    
+}
+
+extension QuestionViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.rightAnswers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell", for: indexPath)
+        cell.textLabel?.text = self.rightAnswers[indexPath.row]
+        
+        return cell
+    }
 }
 
